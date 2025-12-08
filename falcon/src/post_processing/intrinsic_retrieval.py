1d3303975f95a25589178f8d6aeb400774810c4f
ShouyangDong <shouyang.dong@gmail.com>
2025-12-08 13:36:06 +0800

    fix bm25

--- falcon/src/post_processing/intrinsic_retrieval.py
+++ falcon/src/post_processing/intrinsic_retrieval.py
@@ -1,12 +1,22 @@
-from .src.bm25 import BM25
+import re
+
+from falcon.src.bm25 import BM25
 
 
-def retrieve_documentation(query, doc_path):
-    bm25 = BM25(doc=doc_path)
-    result = bm25.cal_similarity_rank(query)
-    return result
+def retrieve_documentation(query: str):
+    bm25 = BM25(custom_words=[query])
+    ranked_results = bm25.cal_similarity_rank(query)
+    strict_results = []
+
+    for doc, score in ranked_results:
+        if re.search(r"\b" + re.escape(query) + r"($|[^a-zA-Z0-9_])", doc):
+            strict_results.append((doc, score))
+    return ranked_results[0]
 
 
 if __name__ == "__main__":
-    query = "add"
-    doc_path = "./"
+    query = "bang_add"
+    print(retrieve_documentation(query))
+    print("=======")
+    query = "bang_matmul"
+    print(retrieve_documentation(query))
